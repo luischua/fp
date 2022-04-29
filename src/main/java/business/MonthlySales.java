46672eed594fd2c0021eafd5fa20817acb8fbd32
Luis Chua <luis.d.chua@gmail.com>
2022-04-29 15:44:48 +0800

    fix total units bug

--- src/main/java/business/MonthlySales.java
+++ src/main/java/business/MonthlySales.java
@@ -48,6 +48,7 @@ public class MonthlySales extends CouchDocument {
 
         totalOrder =  orderList.size();
         totalValue = new BigDecimal(0);
+        totalUnitsSold = 0;
         for(Order order: orderList){
             totalValue = totalValue.add(order.getTotal());
             if(order.getProducts() != null) {
